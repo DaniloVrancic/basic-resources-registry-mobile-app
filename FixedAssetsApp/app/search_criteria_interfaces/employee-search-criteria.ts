7a4695fbbb0c3d666313b571e0d7dc4f675dd31e
DaniloVrancic <danilov98@hotmail.com>
2024-07-05 19:48:08 +0200

    MP 1.4.19: Added comments on values

--- FixedAssetsApp/app/search_criteria_interfaces/employee-search-criteria.ts
+++ FixedAssetsApp/app/search_criteria_interfaces/employee-search-criteria.ts
@@ -1,5 +1,5 @@
 export interface EmployeeSearchCriteria {
     name?: string;
-    income_min?: number;
+    income_min?: number; //Going to filter in the range between min and max income of a all employees
     income_max?: number;
   }

9262416572bd8f19a765958cf73fee96d81a47ef
DaniloVrancic <danilov98@hotmail.com>
2024-07-09 15:34:08 +0200

    MP 1.5.2: Database queries completed.

--- FixedAssetsApp/db/db.js
+++ FixedAssetsApp/db/db.js
@@ -64,13 +64,65 @@ import {
        ON UPDATE NO ACTION)
     `
 
-    
+    const transferListQuery = `
+    CREATE TABLE IF NOT EXISTS 'transfer_list' (
+    'id' INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY ('id'))
+    `
 
+    const inventoryItemQuery = `
+    CREATE TABLE IF NOT EXISTS 'inventory_item' (
+    'fixed_asset_id' INT NOT NULL,
+    'transfer_list_id' INT NOT NULL,
+    'currentEmployeeId' INT NOT NULL,
+    'new_employee_id' INT NOT NULL,
+    'currentLocationId' INT NOT NULL,
+    'newLocationId' INT NOT NULL,
+    PRIMARY KEY ('fixed_asset_id', 'transfer_list_id'),
+    INDEX 'fk_inventory_item_transfer_list1_idx' ('transfer_list_id' ASC) VISIBLE,
+    INDEX 'fk_inventory_item_employee1_idx' ('currentEmployeeId' ASC) VISIBLE,
+    INDEX 'fk_inventory_item_employee2_idx' ('new_employee_id' ASC) VISIBLE,
+    INDEX 'fk_inventory_item_location1_idx' ('currentLocationId' ASC) VISIBLE,
+    INDEX 'fk_inventory_item_location2_idx' ('newLocationId' ASC) VISIBLE,
+     CONSTRAINT 'fk_inventory_item_fixed_asset1'
+       FOREIGN KEY ('fixed_asset_id')
+       REFERENCES 'my_fixed_assets'.'fixed_asset' ('id')
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT 'fk_inventory_item_transfer_list1'
+       FOREIGN KEY ('transfer_list_id')
+       REFERENCES 'my_fixed_assets'.'transfer_list' ('id')
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT 'fk_inventory_item_employee1'
+       FOREIGN KEY ('currentEmployeeId')
+       REFERENCES 'my_fixed_assets'.'employee' ('id')
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT 'fk_inventory_item_employee2'
+       FOREIGN KEY ('new_employee_id')
+       REFERENCES 'my_fixed_assets'.'employee' ('id')
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT 'fk_inventory_item_location1'
+       FOREIGN KEY ('currentLocationId')
+       REFERENCES 'my_fixed_assets'.'location' ('id')
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT 'fk_inventory_item_location2'
+       FOREIGN KEY ('newLocationId')
+       REFERENCES 'my_fixed_assets'.'location' ('id')
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION)
+    `
 
     try {
-      await db.executeSql(locationTableQuery)
-      await db.executeSql(employeeTableQuery)
-      await db.executeSql(fixedAssetTableQuery)
+      await db.executeSql(locationTableQuery);
+      await db.executeSql(employeeTableQuery);
+      await db.executeSql(fixedAssetTableQuery);
+      await db.executeSql(transferListQuery);
+      await db.executeSql(inventoryItemQuery);
+
     } catch (error) {
       console.error(error)
       throw Error(`Failed to create tables`)

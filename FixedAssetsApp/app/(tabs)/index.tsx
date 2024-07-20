0ed8a860e70c209212824d498a62a753a29aaa2d
DaniloVrancic <danilov98@hotmail.com>
2024-07-20 19:38:04 +0200

    MP 1.7.9.10: Edited design

--- FixedAssetsApp/app/(tabs)/index.tsx
+++ FixedAssetsApp/app/(tabs)/index.tsx
@@ -108,7 +108,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 6,
     paddingHorizontal: 16,
-    gap: 8,
+    gap: 9,
     borderBottomColor: 'grey', 
     borderBottomWidth: 2
   },
